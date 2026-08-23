726c57f006aa568e19ea0f4cd6471b36832a413a
Pascal Gauthier <pgauthier@nihilisme.ca>
2026-08-23 13:52:21 -0400

    Refactor tiled2map into cmd package structure
    Update .gitignore to use relative path prefix for the binary.
    Create new cmd/tiled2map/main.go with complete CLI
    implementation including version info, flag parsing, and
    error handling.

---
+++ cmd/tiled2map/main.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/laghoule/tiled2map/internal/pkg/asm"
+	"github.com/laghoule/tiled2map/internal/pkg/atlas"
+	"github.com/laghoule/tiled2map/internal/pkg/tiled"
+)
+
+var (
+	version   = "unknown"
+	gitCommit = "unknown"
+)
+
+func main() {
+	fmt.Printf("Tiled2map version: %s, git commit: %s\n", version, gitCommit)
+	mapFile := flag.String("map", "", "Path to the Tiled map file (JSON format)")
+	destPath := flag.String("dest", ".", "Destination path for the generated files")
+	sceneDimension := flag.String("dimension", "", "Dimension of each scenes in tile (width x height)")
+	filePrefix := flag.String("fileprefix", "master", "Prefix for the generated files")
+	flag.Parse()
+
+	if *mapFile == "" {
+		flag.Usage()
+		exitWithError(fmt.Errorf("map file not specified"))
+	}
+
+	if err := validateDestPath(*destPath); err != nil {
+		exitWithError(err)
+	}
+
+	tileMap, err := tiled.NewMap(*mapFile)
+	if err != nil {
+		exitWithError(err)
+	}
+
+	allGIDs := tiled.GetUniqueGID(tileMap.Layers)
+	tilesInfo := tiled.GetSortedTilesInfo(allGIDs, tileMap.TileSets)
+	gidLocalTIL := tiled.GetGIDToLocalTIL(allGIDs)
+
+	sceneDim, err := asm.ExtractDimension(*sceneDimension)
+	if err != nil {
+		exitWithError(err)
+	}
+
+	if len(tilesInfo) == 0 {
+		exitWithError(fmt.Errorf("no tiles found"))
+	}
+
+	if sceneDim == (asm.Dimension{}) {
+		fmt.Printf("\nScene dimension not specified, turn off scene and tiles attributes creation.\n\n")
+
+	} else {
+		fmt.Printf("Number of scenes: %d\n", (tileMap.Width*tileMap.Height)/(sceneDim.Width*sceneDim.Height))
+		fmt.Printf("Scene dimension: %dx%d\n", sceneDim.Width, sceneDim.Height)
+		fmt.Printf("Scene size: %d bytes\n\n", sceneDim.Width*sceneDim.Height)
+	}
+
+	numTiles := len(tilesInfo)
+	tileSize := tilesInfo[0].Dimension.Width * tilesInfo[0].Dimension.Height
+	tilesetSize := numTiles * tileSize
+	fmt.Printf("Number of tiles: %d\n", numTiles)
+	fmt.Printf("Tile dimension: %dx%d\n", tilesInfo[0].Dimension.Width, tilesInfo[0].Dimension.Height)
+	fmt.Printf("Tile size: %d bytes\n", tileSize)
+	fmt.Printf("Tileset size: %d bytes\n", tilesetSize)
+
+	master, err := atlas.NewMaster(*destPath, *filePrefix, tilesInfo)
+	if err != nil {
+		exitWithError(err)
+	}
+
+	// Create and save the master atlas file
+	err = master.CreateAndSave()
+	if err != nil {
+		exitWithError(err)
+	}
+
+	// Create and save the ASM file with the extracted scene dimension
+	asmLinker := asm.NewASMLinker(*destPath, *filePrefix, tileMap, tilesInfo, gidLocalTIL)
+	err = asmLinker.CreateAndSave(sceneDim)
+	if err != nil {
+		exitWithError(err)
+	}
+
+	fmt.Println("")
+	fmt.Println("Done!")
+}
+
+// validateDestPath validates the destination path for the generated files
+func validateDestPath(destPath string) error {
+	fInfo, err := os.Lstat(destPath)
+	if err != nil {
+		if errors.Is(err, os.ErrNotExist) {
+			return fmt.Errorf("destination path %s does not exist", destPath)
+		}
+		return fmt.Errorf("failed to validate destination path: %v", err)
+	}
+	if !fInfo.IsDir() {
+		return fmt.Errorf("destination path is not a directory")
+	}
+	return nil
+}
+
+// exitWithError prints the error message to standard error and exits the program with a non-zero status code
+func exitWithError(err error) {
+	fmt.Fprintf(os.Stderr, "%v\n", err)
+	os.Exit(1)
+}
